1b640955f64c00c85925941e989d66845df27541
klokMiraj <pudasainimetal96@gmail.com>
2023-02-22 14:33:11 +0000

    minor_fixes_in_authentication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todos": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "test2bd7f28a4": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "testlam": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   },
   "hosting": {
--- src/App.js
+++ src/App.js
@@ -2,37 +2,63 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import { Amplify} from 'aws-amplify';
+///amplify import
+import { Amplify } from 'aws-amplify';
 // import awsconfig from './aws-exports';
 // import { Authenticator } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+
+//importing the rest-api for the app
+// import {API} from 'aws-amplify';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
-  if (!isPassedToWithAuthenticator) {
-    throw new Error(`isPassedToWithAuthenticator was not provided`);
+
+// function App({ isPassedToWithAuthenticator, signOut, user }) {
+//   if (!isPassedToWithAuthenticator) {
+//     throw new Error(`isPassedToWithAuthenticator was not provided`);
+//   }
+
+//   return (
+//     <div className='App'>
+//     <Heading level={1}>Hello {user.username}</Heading>
+//     <Button onClick={signOut}>Sign out</Button>
+//     <h2>Amplify Todos</h2>
+
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className='App'>
+      <div className='App'>
+      <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+    </div>
+        <h2>Amplify Todos</h2>
+      </div>
+    )
   }
-
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
 }
+export default withAuthenticator(App,true);
 
-export default withAuthenticator(App);
-
-export async function getStaticProps() {
-  return {
-    props: {
-      isPassedToWithAuthenticator: true,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       isPassedToWithAuthenticator: true,
+//     },
+//   };
+// }
 
 
 
