a9acedebb4b87a158b3137df88b062bcbb5a68ee
klokMiraj <pudasainimetal96@gmail.com>
2023-02-22 11:23:17 +0000

    with_authenticator_app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import { Amplify} from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { Authenticator } from '@aws-amplify/ui-react';
-
+// import awsconfig from './aws-exports';
+// import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(awsconfig);
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-
-class App extends React.Component {
-
-  render(){
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
-      </header>
-    </div>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
 }
 
-export default App;
+
 
